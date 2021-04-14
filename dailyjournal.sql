eda7176ef7fa3df7b6236b441c31ca38279abb33
kjk1325e <kaitlinjkelley@gmail.comm>
2021-04-14 12:40:04 -0500

    Set up request_handlermodule, models module, and entries package

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -1,7 +1,7 @@
 CREATE TABLE 'Entries' (
     'id'    INTEGER PRIMARY KEY AUTOINCREMENT,
     'date'  DATE,
-    'conept'    VARCHAR,
+    'concept'    VARCHAR,
     'entry'     VARCHAR,
     'mood_id'   INT,
     'instructor_id'     INT,
@@ -32,3 +32,19 @@ CREATE TABLE 'EntryTags' (
     FOREIGN KEY(`entry_id`) REFERENCES `Entries`(`id`),
     FOREIGN KEY(`tag_id`) REFERENCES `Tags`(`id`)
 );
+
+INSERT INTO entries 
+    ('date', 'concept', 'entry', 'mood_id', 'instructor_id')
+    VALUES ('2021-02-01', 'html', 'html is okay', 2, 2);
+
+INSERT INTO Moods
+    ('label')
+    VALUES ('I got this!'),
+            ('Meh'),
+            ('Help!');
+
+INSERT INTO instructors
+    ('first_name', 'last_name')
+    VALUES ('Hannah', 'Hall'),
+            ('Scott', 'Silver'),
+            ('Adam', 'Sheaffer');
